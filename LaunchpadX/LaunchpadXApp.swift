698910b2cd643a738df27d154ab92b737324d1ba
poechiang <poechiang@live.cn>
2026-05-10 17:14:39 +0800

    Initial Commit

---
+++ LaunchpadX/LaunchpadXApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaunchpadXApp.swift
+//  LaunchpadX
+//
+//  Created by Jeffrey Chiang on 2026/5/10.
+//
+
+import SwiftUI
+
+@main
+struct LaunchpadXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
